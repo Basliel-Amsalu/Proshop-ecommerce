189f016edf7069f4795cc30c273389cd30d276ee
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-01-31 23:24:57 +0300

    backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`server running on port ${port}`));
