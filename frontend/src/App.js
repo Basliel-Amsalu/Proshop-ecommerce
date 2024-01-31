ee6c255d87c90a966ec547310b35fc8e5d451a2a
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-01-31 15:58:46 +0300

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1> welcome</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
