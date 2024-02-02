f2126cecef7a072ac50c463f3d31b54e1ea2e727
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-02-02 06:02:52 +0300

    backend authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,18 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import { generateToken } from "../utils/generateToken.js";
 
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
-    res.json({
+    generateToken(res, user._id);
+
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmon: user.isAdmin,
+      isAdmin: user.isAdmin,
     });
   } else {
     res.status(401);
@@ -18,19 +21,67 @@ export const authUser = asyncHandler(async (req, res) => {
 });
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(409);
+    throw new Error("User already exists");
+  } else {
+    const user = await User.create({ name, email, password });
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  }
 });
 
 export const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("user profile");
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update profile");
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  user.name = req.body.name || user.name;
+  user.email = req.body.email || user.email;
+
+  if (req.body.password) {
+    user.password = req.body.password || user.password;
+  }
+
+  const updatedUser = await user.save();
+
+  res.status(200).json({
+    _id: updatedUser._id,
+    name: updatedUser.name,
+    email: updatedUser.email,
+    isAdmin: updatedUser.isAdmin,
+  });
 });
 
 export const getUsers = asyncHandler(async (req, res) => {
